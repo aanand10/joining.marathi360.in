b09487c8eec868156f7608ad44df5bb089f87736
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2021-04-25 11:57:57 +0530

    popup

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
+// const sign_in_btn = document.querySelector("#sign-in-btn");
+// const sign_up_btn = document.querySelector("#sign-up-btn");
+// const container = document.querySelector(".container");
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
+// sign_up_btn.addEventListener("click", () => {
+//   container.classList.add("sign-up-mode");
+// });
 
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+// sign_in_btn.addEventListener("click", () => {
+//   container.classList.remove("sign-up-mode");
+// });
+$("#submit").click(function () {
+  swal("Good job!", "You clicked the button!", "success");
 });
